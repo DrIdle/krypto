f3042be11afe9fe448121a8f61e674d420ec7bd4
DrIdle <jamesborso@gmail.com>
2022-04-15 08:55:07 +0200

    Tests for SHA1 and Main.kt cleanup

---
+++ src/test/kotlin/krypto/hash/SHA1Test.kt
@@ -0,0 +1,28 @@
+package krypto.hash
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@OptIn(ExperimentalUnsignedTypes::class)
+class SHA1Test {
+
+    @Test
+    fun `The hash of the empty should be correct`() {
+        val testString: String = ""
+        val uByteArray = testString.toByteArray(charset = Charsets.US_ASCII).toUByteArray()
+        val sha12 = SHA1()
+        val hash2 = sha12.hash(uByteArray)
+
+        assertEquals("da39a3ee5e6b4b0d3255bfef95601890afd80709", hash2)
+    }
+
+    @Test
+    fun `The hash of the well known string should be correct`() {
+        val testString: String = "The quick brown fox jumps over the lazy dog"
+        val uByteArray = testString.toByteArray(charset = Charsets.US_ASCII).toUByteArray()
+        val sha12 = SHA1()
+        val hash2 = sha12.hash(uByteArray)
+
+        assertEquals("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12", hash2)
+    }
+}
